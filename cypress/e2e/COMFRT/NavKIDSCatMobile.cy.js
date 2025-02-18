6f15d88e4b506c19d24c67a0dadae7c44ea215bf
Angel Arellan <angelarellan@MacBook-Pro.local>
2025-02-18 19:06:03 -0300

    Añadiendo cambiossss

--- cypress/e2e/COMFRT/NavKIDSCatMobile.cy.js
+++ cypress/e2e/COMFRT/NavKIDSCatMobile.cy.js
@@ -1,12 +1,9 @@
-// //Falta validar lo del checkout
-
-
 // describe('Prueba en dispositivos móviles en www.comfrt.com', () => {
 //     const devices = [
 //       { name: 'iPhone 15 Pro Max', width: 430, height: 932 },
 //       /*{ name: 'Samsung Galaxy S24', width: 412, height: 915 }*/
 //     ];
-  
+
 //     devices.forEach((device) => {
 //         it(`Debería abrir el menú hamburguesa y navegar en ${device.name}`, () => {
 //           // Configura la emulación para el dispositivo actual
@@ -39,6 +36,7 @@
 //     .should('be.visible'); // Verifica que es visible
 //     cy.wait(2000)
 
+
 //     // Interactúa con el selector de colores del producto Signature kids Hoodie
 //     cy.get('#featured_products_section-shop-kids-section-draft_2 > div > div.zZYXATlU > div > div:nth-child(1) > div.VtFAkf4R > fieldset > div > label:nth-child(1) > a', { timeout: 10000 })
 //     .should('be.visible')
@@ -61,7 +59,7 @@
 //             .click({ force: true });
     
 //      // Verifica que se abrió la página del producto 
-//         cy.url().should('include', 'products/signature-kids-hoodie?variant=43891268747308');
+//      cy.url().should('include', 'products/signature-kids-hoodie');
 
 //      // Hacer scroll en la página del producto
 //       cy.scrollTo('bottom'); // Desplaza hasta el final de la página
@@ -76,9 +74,9 @@
 //        cy.wait(2000);
 
 //        cy.get('.O9IcGWrR > :nth-child(2) > .v26a1W51 > :nth-child(2) > .N1cHTXJ7 > .XSvSsyWQ', { timeout: 10000 })
-//      .should('be.visible')
-//      .click({ force: true });
-//      cy.wait(2000);
+//       .should('be.visible')
+//        .click({ force: true });
+//        cy.wait(2000);
 
 //      cy.get('.O9IcGWrR > :nth-child(2) > .v26a1W51 > :nth-child(3) > .N1cHTXJ7 > .XSvSsyWQ', { timeout: 10000 })
 //      .should('be.visible')
@@ -120,17 +118,40 @@
 //         .should('be.enabled')
 //         .click({ force: true });
     
-//     //Hacer clic en el botón checkout', () => {
-//       cy.get('#checkout_button-checkout_button_1 > form > button', { timeout: 10000 })
-//       .click({ force: true });
-//       cy.wait(2000)
+//        /* //Hacer clic en el botón checkout', () => {
+//             cy.intercept('POST', '/checkout').as('checkoutRequest'); // Intercepta la solicitud de checkout
+//             cy.get('#checkout_button-checkout_button_1 > form > button').click();
+//             cy.wait('@checkoutRequest'); // Espera que se haga la solicitud
+//             cy.url().should('include', '/checkout'); // Verifica que se haya redirigido*/
+
+
+          
+          
+
+//            /* // Verifica que la página de "Checkout" se haya cargado correctamente
+//     cy.url().should('include', 'https://comfrt.com/checkouts/cn');
+
+//             // Desplázate y espera
+//     cy.scrollTo('bottom');
+//     cy.wait(2000);
+//     cy.scrollTo('center');
+//     cy.wait(2000);
+//     cy.scrollTo('top');
+//     cy.wait(2000);   */
 
 //       // Vuelve a la página anterior
-//       cy.go('back'); 
-//       cy.url().should('include', 'products/signature-kids-hoodie?variant=43891268747308'); // Verifica que regresó correctamente
-// }
+//       //cy.go('back'); 
+//       //cy.url().should('include', 'products/signature-kids-hoodie?variant=43891268747308'); // Verifica que regresó correctamente
+
+
+//     }
+//  });
 //   });
-//       });
-//     });
-//   });
-  
+// });
+// });
+
+
+// //Revisar el checkout+ cómo bloquear pop up 
+
+
+
