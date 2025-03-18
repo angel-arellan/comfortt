3778ac1741921471f1b4c3d6d5711f9932f4d58f
Angel Arellan <angelarellan@MacBook-Pro.local>
2025-03-17 21:59:41 -0300

    ultimos camb

--- cypress.config.js
+++ cypress.config.js
@@ -1,41 +1,27 @@
 const { defineConfig } = require('cypress');
-const mochawesomeMerge = require('mochawesome-merge');
-const mochawesomeReportGenerator = require('mochawesome-report-generator');
 
 module.exports = defineConfig({
-  projectId: 'yaesxf',
   e2e: {
     setupNodeEvents(on, config) {
-      // Registra el plugin de Mochawesome
-      require('cypress-mochawesome-reporter/plugin')(on);
-
-      // Fusión de los resultados JSON después de ejecutar las pruebas
-      on('after:run', (results) => {
-        const mergedJson = mochawesomeMerge(results.runs);
-        mochawesomeReportGenerator.create(mergedJson, {
-          reportDir: 'mochawesome-report', // Carpeta de salida
-          reportFilename: 'mochawesome-report.html', // Nombre del archivo HTML
-          overwrite: false, // No sobrescribir los archivos existentes
-          html: true, // Asegurarse de que se genere el archivo HTML
-          json: true, // Asegurarse de que se genere el archivo JSON
-        });
-      });
+      // Implementa eventos aquí si es necesario
     },
-    baseUrl: 'https://www.comfrt.com',
-    supportFile: 'cypress/support/e2e.js',
-    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-    reporter: 'cypress-mochawesome-reporter',
+    reporter: "mochawesome",
     reporterOptions: {
-      reportDir: 'mochawesome-report', // Carpeta de salida de los reportes
-      overwrite: false, // No sobrescribir archivos
-      html: true, // Habilitar la generación del archivo HTML
-      json: true, // Habilitar la generación del archivo JSON
-    },
+      reportDir: "mochawesome-report",
+      overwrite: false,
+      html: true,
+      json: true
+    }
 
-    experimentalSessionAndOrigin: true,
+    ,
+
+   // experimentalSessionAndOrigin: true,
     chromeWebSecurity: false,
   },
 
   video: true,
   videosFolder: 'cypress/videos',
-});
+  }
+
+  
+);
