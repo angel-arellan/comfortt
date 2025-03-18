fe64814994e1c70ce1cfb3652bdb535adbaf8ca7
Angel Arellan <angelarellan@MacBook-Pro.local>
2025-03-18 08:27:29 -0300

    cambios 18-03

--- cypress.config.js
+++ cypress.config.js
@@ -1,27 +1,50 @@
-const { defineConfig } = require('cypress');
-
 module.exports = defineConfig({
+  video: true, // Habilita la grabación de video
+  videosFolder: 'cypress/videos', // Carpeta donde se guardan los videos
   e2e: {
     setupNodeEvents(on, config) {
-      // Implementa eventos aquí si es necesario
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
-    reporter: "mochawesome",
+    reporter: 'cypress-mochawesome-reporter',
     reporterOptions: {
-      reportDir: "mochawesome-report",
+      reportDir: 'mochawesome-report',
       overwrite: false,
       html: true,
-      json: true
-    }
-
-    ,
-
-   // experimentalSessionAndOrigin: true,
-    chromeWebSecurity: false,
+      json: true,
+      embeddedScreenshots: true, // Incrusta capturas en el HTML
+      video: true, // Agrega enlaces a los videos en el reporte
+    },
   },
+});
 
-  video: true,
-  videosFolder: 'cypress/videos',
-  }
+
+
+
+
+// const { defineConfig } = require('cypress');
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // Implementa eventos aquí si es necesario
+//     },
+//     reporter: "mochawesome",
+//     reporterOptions: {
+//       reportDir: "mochawesome-report",
+//       overwrite: false,
+//       html: true,
+//       json: true
+//     }
+
+//     ,
+
+//    // experimentalSessionAndOrigin: true,
+//     chromeWebSecurity: false,
+//   },
+
+//   video: true,
+//   videosFolder: 'cypress/videos',
+//   }
 
   
-);
+// );
